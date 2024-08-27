f675b04bb47410e7d4c32842334d154d58f95f73
NekoNyangYee <78199616+NekoNyangYee@users.noreply.github.com>
2024-08-27 20:55:32 +0900

    add: 숫자 스타일 선택 관련 알고리즘 추가

---
+++ etc/string.js
@@ -0,0 +1,54 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("✨숫자 매크로 세상에 오신걸 환영합니다!!✨\n원하시는 문자열을 넣어주세요👉👉 ", (string) => {
+    if (!string) {
+        console.log("입력된 문장이 없습니다.");
+        rl.close();
+        return;
+    }
+
+    rl.question("원하시는 숫자 스타일을 고르세요: '꽉찬원', '빈원', '괄호', '점'\n", (style) => {
+        enumString(string, style);
+        rl.close();
+    });
+});
+
+function enumString(input, style) {
+    const sentences = input.match(/[^.?!]+[.?!]/g).map(sentence => sentence.trim());
+    const circleFillNumbers = ['❶', '❷', '❸', '❹', '❺', '❻', '❼', '❽', '❾', '❿'];
+    const circleNumbers = ['①', '②', '③', '④', '⑤', '⑥', '⑦', '⑧', '⑨', '⑩'];
+
+    console.log('==================문장 분리 중=====================');
+    console.log(sentences);
+    console.log('==================================================');
+
+    setTimeout(() => {
+        console.log('=====================출력 결과====================');
+        for (let i = 0; i < sentences.length; i++) {
+            if (style === '꽉찬원') {
+                console.log(`${circleFillNumbers[i]} ${sentences[i]}`);
+            } else if (style === '빈원') {
+                console.log(`${circleNumbers[i]} ${sentences[i]}`);
+            } else if (style === '괄호') {
+                console.log(`(${i + 1}) ${sentences[i]}`);
+            } else if (style === '점') {
+                console.log(`${i + 1}. ${sentences[i]}`);
+            }
+            else {
+                console.log(`잘못된 선택입니다. 기본 스타일인 꽉찬원으로 사용합니다.`);
+                console.log(`${circleFillNumbers[i]} ${sentences[i]}`);
+            }
+        }
+        console.log('==================================================');
+        console.log('위에 출력된 문장들을 복사해가세요!!');
+
+        if (style === '꽉찬원' || style === '빈원') {
+            console.log(`🔱 현재 선택하신 ${style}은 현재 10번까지밖에 지원하지 않아요😭 10번이 넘어갈 경우 숫자가 표시되지 않으니 원하시면 배열을 수정해주세요❗❗`);
+        }
+    }, 2000);
+}
