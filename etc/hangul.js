034471e55e3c176f69e25819d89b9f031c2272b4
NekoNyangYee <78199616+NekoNyangYee@users.noreply.github.com>
2024-09-20 23:13:15 +0900

    fix: 한글 초/중/종성 표시로 변경

--- etc/hangul.js
+++ etc/hangul.js
@@ -3,6 +3,22 @@ function hangulSplit(str) {
     let jungSeong = [];
     let jongSeong = [];
 
+    const choSeongList = [
+        "ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ",
+        "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"
+    ];
+
+    const jungSeongList = [
+        "ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ",
+        "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"
+    ];
+
+    const jongSeongList = [
+        "", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ",
+        "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ",
+        "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"
+    ];
+
     for (let i = 0; i < str.length; i++) {
         const charAt = str.charCodeAt(i);
         const diff = charAt - 0xAC00;
@@ -12,15 +28,15 @@ function hangulSplit(str) {
             const jungSeongUnicode = Math.floor((diff % 588) / 28);
             const jongSeongUnicode = diff % 28;
 
-            choSeong.push(choSeongUnicode);
-            jungSeong.push(jungSeongUnicode);
-            jongSeong.push(jongSeongUnicode);
+            choSeong.push(choSeongList[choSeongUnicode]);
+            jungSeong.push(jungSeongList[jungSeongUnicode]);
+            jongSeong.push(jongSeongList[jongSeongUnicode]);
         }
     }
     return {
-        choSeong: choSeong.length,
-        jungSeong: jungSeong.length,
-        jongSeong: jongSeong.filter(e => e !== 0).length
+        choSeong,
+        jungSeong,
+        jongSeong: jongSeong.filter(e => e !== '')
     };
 }
-console.log(hangulSplit("아반"));
+console.log(hangulSplit("꽭뚫아"));
